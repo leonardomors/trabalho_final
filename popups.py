916fce1d42c8d9e9035fc9027fd4c6da9cda1a2f
Leonardo_Moraes <74950357+leonardomors@users.noreply.github.com>
2022-08-11 14:24:11 -0300

    Add files via upload

--- popups.py
+++ popups.py
@@ -1,60 +1,28 @@
-
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy_garden.graph import LinePlot
 from kivy.uix.boxlayout import BoxLayout
-from timeseriesgraph import TimeSeriesGraph
-
 
 class ModbusPopup(Popup):
     """
-    popup configuracao modbus
+    Popup da tela de conexção (modbus)
     """
-    _info_lb = None
     def __init__(self, server_ip, port, **kwargs):
         super().__init__(*kwargs)
         self.ids.txt_ip.text = str(server_ip)
         self.ids.txt_port.text = str(port)
-    
-    def setInfo(self, message):
-        self._info_lb = Label(text = message)
-        self.ids.layout.add_widget(self._info_lb)
 
-    def clearInfo(self):
-        if self._info_lb is not None:
-            self.ids.layout.remove_widget(self._info_lb)     
+    def set_info(self, message):
+        self._info = Label(text= str(message), font_size=15, size_hint= (1,0.3))
+        self.ids.layout.add_widget(self._info)
+
+  #  def clear_info(self):
+   #     if self._info is not None:
+    #        self.ids.layout.remove_widget(self._info)
+        
+
 
 class ScanPopup(Popup):
-    """
-    popup varredura
-    """ 
-    def __init__(self, scantime, **kwargs):
-        """
-        construtor scanpopup
-        """
+    def __init__(self,scantime, **kwargs):
         super().__init__(**kwargs)
-        self.ids.txt_st.text = str(scantime)
-
-class DataGraphPopup(Popup):
-   def __init__(self, xmax, plot_color, **kwargs):
-       super().__init__(**kwargs)
-       self.plot = LinePlot(line_width=1.5, color= plot_color)
-       self.ids.graph.add_plot(self.plot)
-       self.ids.graph.xmax = xmax
-
-class LabelCheckBoxDataGraph(BoxLayout):
-    pass
-
-class HistGraphPopup(Popup):
-    def __init__(self, **kwargs):
-        super().__init__()
-        for key, value in kwargs.get('tags').items():
-            cb = LabelCheckBoxHistGraph()
-            cb.ids.label.text = key
-            cb.ids.label.color = value['color']
-            cb.id = key
-            self.ids.sensores.add_widget(cb)
-
-
-class LabelCheckBoxHistGraph(BoxLayout):
-    pass
+        self.ids.ti_scan.text = str(scantime)
